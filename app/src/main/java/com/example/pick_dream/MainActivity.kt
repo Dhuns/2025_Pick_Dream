4baf484527d1a816bee803d953a45cc930010d1a
Jossi02 <02love326@kyonggi.ac.kr>
2025-05-05 14:14:21 +0900

    MainActivity 수정

--- app/src/main/java/com/example/pick_dream/MainActivity.kt
+++ app/src/main/java/com/example/pick_dream/MainActivity.kt
@@ -22,13 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // 툴바를 앱 전체의 기본 ActionBar로 설정
-        setSupportActionBar(binding.mainToolbar)
+        // setSupportActionBar(binding.mainToolbar)
 
         // 기본 Fragment 설정
         supportFragmentManager.beginTransaction()
             .replace(R.id.nav_host_fragment_activity_main, HomeFragment())
             .commit()
-        supportActionBar?.title = "홈"
+        // supportActionBar?.title = "홈"
 
         val navView: BottomNavigationView = binding.navView
 
@@ -38,28 +38,25 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main, HomeFragment())
                         .commit()
-                    supportActionBar?.title = "홈"
-                    // 커스텀 타이틀 제거
-                    binding.mainToolbar.removeAllViews()
+                    // supportActionBar?.title = "홈"
+                    // binding.mainToolbar.removeAllViews()
                     true
                 }
                 R.id.navigation_favorite -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main, FavoriteFragment())
                         .commit()
-                    // 커스텀 타이틀 중앙에 표시
-                    binding.mainToolbar.removeAllViews()
-                    val customTitle = LayoutInflater.from(this).inflate(R.layout.toolbar_title, binding.mainToolbar, false)
-                    binding.mainToolbar.addView(customTitle)
+                    // binding.mainToolbar.removeAllViews()
+                    // val customTitle = LayoutInflater.from(this).inflate(R.layout.toolbar_title, binding.mainToolbar, false)
+                    // binding.mainToolbar.addView(customTitle)
                     true
                 }
                 R.id.navigation_mypage -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main, MypageFragment())
                         .commit()
-                    supportActionBar?.title = "마이페이지"
-                    // 커스텀 타이틀 제거
-                    binding.mainToolbar.removeAllViews()
+                    // supportActionBar?.title = "마이페이지"
+                    // binding.mainToolbar.removeAllViews()
                     true
                 }
                 else -> false
