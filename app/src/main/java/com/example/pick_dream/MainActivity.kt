e8164a8f2d3a16cf584fc41149fe905b55220080
Jossi02 <02love326@kyonggi.ac.kr>
2025-05-05 15:14:04 +0900

    하단 네비게이션 바 수정
    커서 무료 끝나서 큰일 남

--- app/src/main/java/com/example/pick_dream/MainActivity.kt
+++ app/src/main/java/com/example/pick_dream/MainActivity.kt
@@ -31,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         // supportActionBar?.title = "홈"
 
         val navView: BottomNavigationView = binding.navView
+        
+        // 초기 아이콘 설정
+        navView.menu.findItem(R.id.navigation_home).setIcon(R.drawable.ic_home_filled)
+        navView.menu.findItem(R.id.navigation_favorite).setIcon(R.drawable.ic_favorite)
+        navView.menu.findItem(R.id.navigation_mypage).setIcon(R.drawable.ic_mypage)
 
         navView.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -38,25 +43,30 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main, HomeFragment())
                         .commit()
-                    // supportActionBar?.title = "홈"
-                    // binding.mainToolbar.removeAllViews()
+                    // 아이콘 변경
+                    navView.menu.findItem(R.id.navigation_home).setIcon(R.drawable.ic_home_filled)
+                    navView.menu.findItem(R.id.navigation_favorite).setIcon(R.drawable.ic_favorite)
+                    navView.menu.findItem(R.id.navigation_mypage).setIcon(R.drawable.ic_mypage)
                     true
                 }
                 R.id.navigation_favorite -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main, FavoriteFragment())
                         .commit()
-                    // binding.mainToolbar.removeAllViews()
-                    // val customTitle = LayoutInflater.from(this).inflate(R.layout.toolbar_title, binding.mainToolbar, false)
-                    // binding.mainToolbar.addView(customTitle)
+                    // 아이콘 변경
+                    navView.menu.findItem(R.id.navigation_home).setIcon(R.drawable.ic_home)
+                    navView.menu.findItem(R.id.navigation_favorite).setIcon(R.drawable.ic_favorite_filled)
+                    navView.menu.findItem(R.id.navigation_mypage).setIcon(R.drawable.ic_mypage)
                     true
                 }
                 R.id.navigation_mypage -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main, MypageFragment())
                         .commit()
-                    // supportActionBar?.title = "마이페이지"
-                    // binding.mainToolbar.removeAllViews()
+                    // 아이콘 변경
+                    navView.menu.findItem(R.id.navigation_home).setIcon(R.drawable.ic_home)
+                    navView.menu.findItem(R.id.navigation_favorite).setIcon(R.drawable.ic_favorite)
+                    navView.menu.findItem(R.id.navigation_mypage).setIcon(R.drawable.ic_mypage_filled)
                     true
                 }
                 else -> false
